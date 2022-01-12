121cbb76e2123f076212dddf9f7de7149f9a2552
deku <adedokunoluwaferanmi@gmail.com>
2022-01-12 17:54:13 +0100

    add: section 1 which contains inforamtion about tokens swap and liquidity

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from './components/Footer'
 import Hero from "./components/Hero";
-{/*
 import Section1 from './components/Section1';
+{/*
+
 import Section2 from './componennts/Section2';
 import Section3 from './components/Section3'
 */}
@@ -14,8 +15,8 @@ function App() {
       <div className="App-body">
         <div className="Inner-App-body">
           <Hero />
-          {/*
           <Section1 />
+          {/*          
           <Section2 />
           <Section3 />
           */}
--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import styles from '../styles/Section1.module.css'
+import SwapTokenUI from '../assets/swap-tokens-ui.png'
+import LiquidityOptionsUI from '../assets/liquidity-options.png'
 const Section1 =() => {
   return (
-    <div>
-      <h2>this is section 1 after the hero section</h2>
+    <div className={styles.Section1}>
+      <div className={styles.Section1InnerContainer}>
+        <div>
+          <img alt="swap tokens" src={SwapTokenUI} />
+        </div>  
+        <div>
+          <h2 className={styles.Section1MainText}>
+            <span>Swap</span> your tokens and provide <span>liquidity</span> in an instant
+          </h2>
+          <img alt="swap tokens" src={LiquidityOptionsUI} />
+        </div>
+      </div>      
     </div>
   )
 }
