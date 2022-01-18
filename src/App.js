b23bdc47ba8fb1181d2bca7b9752425f95f442f3
Oluwaferanmi-deku <adedokunoluwaferanmi@gmail.com>
2022-01-18 03:43:38 +0100

    feat: trade anytime section, desktop view

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import Footer from './components/Footer'
 import Hero from "./components/Hero";
 import Section1 from './components/Section1';
 import Section2 from './components/Section2';
-{/*
 import Section3 from './components/Section3'
-*/}
+
 function App() {
   return (
     <div className="App">
@@ -16,8 +15,9 @@ function App() {
           <Hero />
           <Section1 />
           <Section2 />
-          {/*                    
           <Section3 />
+          {/*                    
+          
           */}
         </div>
       </div>
--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-
+import React from "react";
+import { ReactComponent as TradeBg } from "../assets/trade-cut-bg.svg";
+import styles from "../styles/Section3.module.css";
+import BnbCoin from "../assets/bnb-coin.png";
+import BtcCoin from "../assets/btc-coin.png";
+import PancakeCoin from "../assets/pancake-coin.png";
 
 const Section3 = () => {
   return (
-    <>
-      <h3>this is section 3</h3>
-    </>
-  )
+    <div className={styles.Section3}>
+      <div>
+        <TradeBg className={styles.TradeBg} />
+      </div>
+      <div className={styles.Section3Content}>
+        <div className={styles.Section3Writeup}>
+          <h3>
+            <span className={styles.TextHighlighted}>Trade</span> anything. No
+            registration, no hassle.
+          </h3>
+          <p>
+            Trade any token on Binance Smart Chain in seconds, just by
+            connecting your wallet.
+          </p>
+          <button className={styles.HeroButtonPrimary}>Trade Now</button>
+        </div>
+        <div className={styles.Section3Coins}>
+          <img src={PancakeCoin} className={styles.PancakeCoin} />
+          <img src={BtcCoin} className={styles.BtcCoin} />
+          <img src={BnbCoin} className={styles.BnbCoin} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Section3;
